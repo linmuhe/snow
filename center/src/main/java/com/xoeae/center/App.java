d944fdff28c5ab13992f3a9baa5ee3efb084a89c
joc <you@example.com>
2018-08-02 18:21:26 +0800

    x

---
+++ center/src/main/java/com/xoeae/center/App.java
@@ -0,0 +1,18 @@
+package com.xoeae.center;
+
+import com.xoease.snowstorm.config.Snow;
+import com.xoease.snowstorm.conn.SnowServer;
+import com.xoease.snowstorm.util.PropertyUtil;
+import org.apache.thrift.transport.TSSLTransportFactory;
+import org.apache.thrift.transport.TServerTransport;
+
+/**
+ *  中心服务器 被serv调用 存放一些中心全局数据
+   以及服务调用
+ //https://www.programcreek.com/java-api-examples/?api=org.eclipse.jetty.servlet.ServletHandler
+ //https://thrift.apache.org/tutorial/java
+ http://thrift.apache.org/tutorial/
+  */
+public class App {
+
+}
--- serv/src/main/java/com/xoeae/dnd/App.java
+++ serv/src/main/java/com/xoeae/dnd/App.java
@@ -1,4 +1,4 @@
-package com.dnd;
+package com.xoeae.dnd;
 
 import com.xoease.snowstorm.config.Snow;
 import com.xoease.snowstorm.conn.SnowServer;
@@ -11,9 +11,17 @@ public class App {
         Snow.PrivateKey =PropertyUtil.getProperty("privateKey");
         Snow.port =Integer.valueOf(PropertyUtil.getProperty("port"));
         Snow.host =PropertyUtil.getProperty("host");
+        System.out.println("############## Server start ###############");
 
+        /**
+         * 启动IM服务
+         */
         SnowServer ser = new SnowServer();
         ser.setNoneSendDataIdle(true);
         ser.start();
+
+
+        System.out.println("############## Server started ###############");
+        ser.join();
     }
 }
